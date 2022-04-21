3642b364f91797991f797e52529508c58cffbc42
midoritiba <mic.tiba@gmail.com>
2022-04-21 09:13:34 +0200

    sidebar and template for dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar';
-import { Col, Row, Container } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import MainDashboardContent from '../components/MainDashboardContent'
 
 const Dashboard = () => {
   return (
     <div className='my-3' style={{backgroundColor: 'rgb(242, 242, 242)'}}>
-      <Container fluid>
-        <Row>
-            <Col className='dashboard-menu col-md-3 col-lg-2'>
+      <Container fluid className='d-flex'>
+          <Sidebar />
+          <MainDashboardContent/>        
+        {/* <Row>
+            <Col className='dashboard-menu col-md-4 col-lg-3'>
                 <Sidebar />
             </Col>
-            <Col className='dashboard-main col-md-9 col-lg-10 pt-2'>
+            <Col className='dashboard-main col-md-8 col-lg-9 pt-2'>
                 <MainDashboardContent/>
             </Col>
-        </Row>
+        </Row> */}
         </Container>
     </div>
   )
